2dda919418a3ffe4cc585e3fb2b88683723478b0
Thomas <camdent@iastate.edu>
2017-01-17 17:09:36 -0600

    fixed dot

---
+++ dot.c
@@ -0,0 +1,11 @@
+double dot(double a[], double b[], int length) {
+	int i=0;
+	double sum;  
+	double returnVal;
+	for (i=0; i<= length; i++) { 
+		sum = sum + a[i] * b[i];
+		}
+		returnVal=sum;
+		sum=0;
+	return returnVal;
+	}
